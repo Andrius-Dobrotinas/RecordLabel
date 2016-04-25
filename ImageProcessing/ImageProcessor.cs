66433524717a2816cdaabdda196dc22a01fee467
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2016-04-25 20:18:56 +0300

    minor refactoring

--- ImageProcessing/ImageProcessor.cs
+++ ImageProcessing/ImageProcessor.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace RecordLabel.ImageProcessing
 {
-    public class ImageProcessor
+    public static class ImageProcessor
     {
         public const string EncodedFileExtension = "jpg";
 

adcea9f1337b80de6781e28e5c0c04521daab3fe
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2016-10-17 18:42:26 +0300

    ...missed these two with the previous commit

--- ContentModels/Models/Reference.cs
+++ ContentModels/Models/Reference.cs
@@ -5,18 +5,6 @@ namespace RecordLabel.Data.Models
 {
     public class Reference : ReferenceBase
     {
-        /*public virtual IList<MainContent> Owners
-        {
-            get
-            {
-                return owners ?? (owners = new List<MainContent>());
-            }
-            set
-            {
-                owners = value;
-            }
-        }*/
-
         public MainContent Owner { get; set; }
 
         private IList<MainContent> owners;
--- ContentModels/Models/ReferenceBase.cs
+++ ContentModels/Models/ReferenceBase.cs
@@ -6,9 +6,6 @@ namespace RecordLabel.Data.Models
 {
     public abstract class ReferenceBase : FirstBase
     {
-        /*[Key]
-        public virtual int Id { get; set; }*/
-
         [Required]
         public string Target { get; set; }
 
